68e7bb064c7b125dba068b4655479ff735cc853b
Doug Hoff <email@doughoff.com>
2017-09-26 14:15:42 -0500

    update

--- functions/birthday-cake.js
+++ functions/birthday-cake.js
@@ -5,20 +5,32 @@ function makeCake(multiplier){
     console.info("Made a cake with " + multiplier + " layers.");
     return cake;
 }
-function frostCake(cake){
+function frost(cake){
     cake = cake || "a basic cake";
     var frostedCake = cake + " with frosting";
     console.info("Frosted the " + cake);
     return frostedCake;
 }
+
+function addCandlesTo(cake, numberOfCandles){
+    cake = cake || "a basic cake";
+    numberOfCandles = numberOfCandles || 21;
+    var candledCake = cake + " with " + numberOfCandles  + " candles";
+}
+
 // use in browser only - values only
-makeCake();
-makeCake(2);
+makeCake()
+makeCake(2)
+frostCake(aCake)
+addCandlesTo(aFrostedCake)
 
 // use for script in web page
 console.log(makeCake(2));
 
-frostCake(makeCake(3));
+frost(makeCake(3));
 var aCake = makeCake(4);
 var aFrostedCake = frostCake(aCake);
 console.log(aFrostedCake);
+
+var birthdayCake = addCandlesTo(aFrostedCake);
+console.log(birthdayCake);
--- functions/lasagna.js
+++
@@ -1,6 +0,0 @@
-// lasagna.js
-function makeLasagna() {
-    // do stuff here to make it ...
-    var someLasagna = 'Italian Sausage Lasagna';
-    return someLasagna;
-}
