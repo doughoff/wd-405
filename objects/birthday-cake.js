59a95a781cd6f77ffa7c89f7ff24bae83459c3d8
Doug Hoff <email@doughoff.com>
2017-09-27 09:36:15 -0500

    update

--- objects/birthday-cake.js
+++ objects/birthday-cake.js
@@ -1,13 +1,16 @@
 // birthday-cake.js
 
+// you can start with an empty object
 var cake = {};
 cake.layers = 3;
 cake.flavor = 'chocolate';
 
+// create other objects using object initialization
 var frosting = {flavor:'chocolate fudge', depthInInches: 1};
 var candles  = {number: 20, isLit: true, sparkle: function(){}};
+// overwrite the function stub
 candles.sparkle = function (){
-    if (this.isLit) {                    // this means the object before the function name i.e. candles
+    if (this.isLit) {       // "this" means the object before the function name i.e. candles
         console.log('I\'m sparkling.');
     } else {
         console.log('not sparkling');
