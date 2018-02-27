094081f8b453690a6087bd6f280a50fa97cfeac6
Doug Hoff <email@doughoff.com>
2018-02-27 14:01:10 -0600

    update

--- functions/math.js
+++ functions/math.js
@@ -1,9 +1,12 @@
+// math.js
 // Math contains many utility functions
+
 console.log(Math.sqrt(4));
 console.log(Math.random());
 console.log(Math.pow(2,8));
 console.log(Math.round(2.4));
 console.log(Math.abs(-2.7));
-// console.log(Math contains several reference values
+
+// Math contains several reference values (constants)
 console.log(Math.E);
 console.log(Math.PI);
