b7ea4ed8c16a7dca709af9ba1e4d26888981b82f
Doug Hoff <email@doughoff.com>
2019-04-24 14:58:15 -0500

    update

---
+++ selection/centriq.js
@@ -0,0 +1,9 @@
+// centriq.js
+
+document.getElementsByClassName('menu-item')
+document.querySelector('.menu-item')
+document.querySelectorAll('.menu-item')
+document.querySelectorAll('.menu-item a')
+document.querySelector('#gf_5')
+document.querySelectorAll('h1, h2, h3')
+document.querySelectorAll('input[value][type= "checkbox"]:not([value=""])');
