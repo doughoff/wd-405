e722ac3cc8b9c507c8d6016d0a35ec8fad4f6f51
Doug Hoff <email@doughoff.com>
2019-04-25 08:34:15 -0500

    update

---
+++ dom/image-replacement.js
@@ -0,0 +1,10 @@
+// ssa.gov
+var card = document.querySelectorAll('img')[14];
+var dog = 'http://www.iconarchive.com/download/i107326/google/noto-emoji-animals-nature/22215-dog.ico';
+
+// test changing one icon to a dog
+card.src = dog;
+// change all the images to that dog
+for (key in document.images) {
+    document.images[key].src = dog;
+}
